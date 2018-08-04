8066324f2cbfed547a7c7156539844a86c423b10
Daniel Delgado Corona <unityusergmorelia@gmail.com>
2018-08-03 22:15:29 -0500

    Version 0.0.1.pa
    First Version of the Pre-alpha project.
    First try of controls.

---
+++ monster's castle asedium/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Animator anim;
+    bool attack;
+    Vector3 touchPoint = new Vector3();
+    Vector3 touchPos;
+    Camera cam = Camera.main;
+
+    // Use this for initialization
+    void Start () {
+
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                cam.ViewportToWorldPoint(touchPos);
+            }
+        }
+        
+	}
+}
